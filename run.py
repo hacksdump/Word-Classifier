527db769da3a2ebb0ad0fd31e6bcd62207a323b6
Harshit Maurya <hmaurya999@gmail.com>
2019-10-18 11:45:56 +0530

    create shell script to automate weekly assignment tasks

---
+++ run.py
@@ -0,0 +1,10 @@
+#!./env/bin/python
+
+################################
+########### WEEK 1 #############
+################################
+
+# Problem statement: Bring the corpus into required format. (preprocessing)
+# word1_tag
+# word2_tag
+import tag_words
