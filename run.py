4a8d3863dc42621ae6df7ec4eeec7ff0089c17af
Harshit Maurya <hmaurya999@gmail.com>
2019-10-18 12:38:15 +0530

    run: add week two tasks to run script

--- run.py
+++ run.py
@@ -8,3 +8,12 @@
 # word1_tag
 # word2_tag
 import tag_words
+
+
+################################
+############ WEEK 2 ############
+################################
+
+# Problem statement: Create a dictionary having entry for unique word+tag combination with it's frequency
+# count in the corpus.
+import word_tag_frequency
