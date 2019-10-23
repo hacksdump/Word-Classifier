67adf68992d289356e2add1e220c570cda21307a
Harshit Maurya <hmaurya999@gmail.com>
2019-10-23 15:35:30 +0530

    plot: full screen

--- word_tag_statistics.py
+++ word_tag_statistics.py
@@ -8,11 +8,16 @@ top_ten_words = word_tag_stats.words_descending[:10]
 top_ten_word_count = [word_tag_stats.word_count[word]
                       for word in top_ten_words]
 print("Top 10 words: ", top_ten_words)
+plot_manager = plt.get_current_fig_manager()
+plot_manager.full_screen_toggle()
 plt.pie(top_ten_word_count, labels=top_ten_words)
+
 plt.show()
 
 top_ten_tags = word_tag_stats.tags_descending[:10]
 top_ten_tag_count = [word_tag_stats.tag_count[tag] for tag in top_ten_tags]
 print("Top 10 tags: ", (word_tag_stats.tags_descending[:10]))
 plt.pie(top_ten_tag_count, labels=top_ten_tags)
+plot_manager = plt.get_current_fig_manager()
+plot_manager.full_screen_toggle()
 plt.show()
