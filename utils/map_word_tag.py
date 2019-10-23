af609376010be00fac1d5a6a41017f0acc186f59
Harshit Maurya <hmaurya999@gmail.com>
2019-10-23 10:31:21 +0530

    add method to read word tag frequency from existing file

--- utils/map_word_tag.py
+++ utils/map_word_tag.py
@@ -1,3 +1,6 @@
+from config.paths import WORD_TAG_FREQUENCY_FILE
+
+
 def get_word_tag_map(preprocessed_file):
     word_tag__frequency = {}
     with open(preprocessed_file) as preprocessed_file:
@@ -11,3 +14,15 @@ def get_word_tag_map(preprocessed_file):
                 word_tag__frequency[key] = 1
 
     return word_tag__frequency
+
+
+def read_word_tag_frequency_file():
+    word_tag__frequency_map = {}
+    with open(WORD_TAG_FREQUENCY_FILE) as word_tag__frequency:
+        for line in word_tag__frequency:
+            line = line.replace("\n", "")
+            word_tag, frequency = line.split(' ')
+            frequency = int(frequency)
+            word, tag = word_tag.split('_')
+            word_tag__frequency_map[(word, tag)] = frequency
+    return word_tag__frequency_map
